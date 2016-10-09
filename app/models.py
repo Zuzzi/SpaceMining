88807668babce87754f213970a90819f7d08b21e
Carlo Peroni <carloperoni.italy@gmail.com>
2016-10-09 12:38:51 +0200

    Added  Flask-Login feature for user sessions

--- app/models.py
+++ app/models.py
@@ -3,15 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class JobPosition(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    job = db.Column(db.String(120))
-    description = db.Column(db.String(120))
-    requirements = db.Column(db.String(120))
-    picture = db.Column(db.String(60))
+	id = db.Column(db.Integer, primary_key=True)
+	job = db.Column(db.String(120))
+	description = db.Column(db.String(120))
+	requirements = db.Column(db.String(120))
+	picture = db.Column(db.String(60))
 
-    def __repr__(self):
-        return '<Job %r>' % (self.job)
-        
+	def __repr__(self):
+		return '<Job %r>' % (self.job)
+		
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +33,24 @@ class User(db.Model):
 	
 	def __repr__(self):
 		return "<User %r>" % (self.username)
+		
+	@property
+	def is_authenticated(self):
+		return True
+
+	@property
+	def is_active(self):
+		return True
+
+	@property
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		try:
+			return unicode(self.id)  # python 2
+		except NameError:
+			return str(self.id)  # python 3
 		
 
 
--- app/views.py
+++ app/views.py
@@ -1,17 +1,21 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, session, url_for, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app
 from .forms import LoginForm
-from app import db, models
+from app import db, models, lm
+from .models import User
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template ("index.html")
 
+
 @app.route("/hiring")
 def hiring():			  
 	job_positions = models.JobPosition.query.all()
 	return render_template("hiring.html", job_positions = job_positions)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -26,11 +30,34 @@ def login():
 				permitted = u.verify_password(request_password)
 				if permitted:
 					print("You have permission to log in")
+					login_user(u)
+					return redirect(url_for("%s_home" % (u.role)))
 				else:
 					print("Wrong PASSWORD!")
 			else:
 				print("Wrong USERNAME!")
-			
-			
-	
 	return render_template("login.html", form = form)
+	
+	
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/miner_home")
+@login_required
+def miner_home():
+	print("I'm in")
+	username = g.user.username
+	return render_template("miner_home.html", username = username)
+	
+	
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+	
+	
+@app.before_request
+def before_request():
+	g.user = current_user
